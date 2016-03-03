f118c26607c9bbd5797f6574533f2f15aa314acc
RadoslavNikolov <RadoslavMNikolov@gmail.com>
2016-03-03 10:47:40 +0200

    init project

---
+++ Public-Orders/src/Public-Orders/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Public_Orders.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
